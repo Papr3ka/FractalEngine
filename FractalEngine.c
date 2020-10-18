7f1d2c21a4baee5cdb2f3560841dcedd68a6ec58
Benjamin <58241833+Papr3ka@users.noreply.github.com>
2020-10-18 12:39:59 -0400

    Update FractalEngine.c

--- FractalEngine.c
+++ FractalEngine.c
@@ -9,6 +9,8 @@ gcc FractalEngine.c -O3 -o FractalEngine.exe
 #include<math.h>
 
 #define GRAYSCALE "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
+#define TERMINAL_WIDTH 236
+#define TERMINAL_HEIGHT 60
 
 int pixel(double x, double y, unsigned long iter, register double complex z,int height, int width){
 	register double realStart = -2.0;
@@ -25,23 +27,18 @@ int pixel(double x, double y, unsigned long iter, register double complex z,int 
 }
 
 int main(){
-	unsigned long count=1;
-	double inc = 0.01;
-	register double start;
-	while(1==1){
-		start += inc;
+	register double complex inc = 0.0025;
+	register double complex start;
+	register double complex wt;
+	while(1){
+		wt += inc;
+		start = ccos(wt)+csin(wt)*I;
 		printf("\033[0;0H");
-		for(int y=0; y<=60;y++){
-			for(int x=0;x<=236;x++){
-				printf("%c", GRAYSCALE[pixel(x,y,150,start,60,236)]);
+		for(int y=0; y<=TERMINAL_HEIGHT;y++){
+			for(int x=0;x<=TERMINAL_WIDTH;x++){
+				printf("%c",GRAYSCALE[pixel(x,y,128,start,TERMINAL_HEIGHT,TERMINAL_WIDTH)]);
 			}
 			printf("\n");
-		}
-		if(start >= (2-abs(inc))){
-			inc *= -1;
-		}
-		if(start <= abs(inc)){
-			inc *= -1;
 		}
 	}
 	return 0;
